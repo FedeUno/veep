0c45f03de825f9e6e1f5a899e6bca5f2d0c8c908
andy <arteysoft@gmail.com>
2022-06-08 11:31:27 -0300

    primer commit

---
+++ clase1/modeloAsincronico.js
@@ -0,0 +1,19 @@
+function estaFuncionEsAsincronica(callBack) {
+    setTimeout(callBack, 0)
+}
+
+function yoSoyElCallBack() {
+   console.log('Estoy dentro del callback')
+}
+
+console.log('vamos a explicar el setTimeout')
+console.log('Paso 1 sincronico ejemplo let a = [1, 2, 3]')
+console.log('Paso 2 sincronico ejemplo if (boolean) {} ')
+console.log('paso 3 asincronico')
+estaFuncionEsAsincronica(yoSoyElCallBack)
+console.log('paso 4 sincronico')
+console.log('paso 5 sincronico')
+for (let x = 6; x < 100; x++) {
+   console.log(`paso ${x} sincronico `)
+}
+
